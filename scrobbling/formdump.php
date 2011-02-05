1838c196399b6fc5140e78df8ad3bd21cf811860
Tim Sell <trsell@gmail.com>
2011-02-05 13:08:15 +0000

    not beta docs

--- scrobbling/formdump.php
+++ scrobbling/formdump.php
@@ -4,5 +4,26 @@
             echo "<li>$h = $v</li>\n";
         else
             echo "<li>$h = $v</li>\n";
-var_dump($_POST); 
+$_POST = array_change_key_case($_POST, CASE_LOWER);
+var_dump($_POST);
+
+foreach (array('artist', 'track', 'timestamp', 'album', 'albumartist', 'duration', 'mbid', 'trackNumber', 'context', 'streamId') as $param) {
+    $$param = $_POST[$param];
+    if (!is_array($$param)) $$param = array($$param);
+    print "$param = ";
+    var_dump($$param);
+}
+$artist=$_POST['artist'];
+var_dump($artist);
+
+for ($i=0; $i<5;$i++){
+    echo "$i\n";
+    if (!isset($artist[$i])) {
+        echo "not set ";
+        echo "trackNumber[{$i}] is not a valid integer.";
+    }
+    var_dump($artist[$i]);
+    
+}
+
 ?>
